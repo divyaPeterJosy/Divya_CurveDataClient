50df4dbb8bda61e310c840083b20e7ec45d61e8d
divyaPeterJosy <divyapeterjosy@gmail.com>
2018-05-23 23:55:39 +0530

    Initial commit

---
+++ PetroClient/Form1.Designer.cs
@@ -0,0 +1,185 @@
+﻿namespace PetroClient
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.submit = new System.Windows.Forms.Button();
+            this.checkBoxContainer = new System.Windows.Forms.Panel();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.curveGridView = new System.Windows.Forms.DataGridView();
+            this.xml_Export = new System.Windows.Forms.Button();
+            this.label3 = new System.Windows.Forms.Label();
+            this.messageBox = new System.Windows.Forms.Label();
+            this.panel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.curveGridView)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(535, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(164, 20);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Clent Socket Program";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(97, 108);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(253, 20);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Select the curves to get the details";
+            // 
+            // submit
+            // 
+            this.submit.Location = new System.Drawing.Point(151, 170);
+            this.submit.Name = "submit";
+            this.submit.Size = new System.Drawing.Size(126, 57);
+            this.submit.TabIndex = 4;
+            this.submit.Text = "Submit";
+            this.submit.UseVisualStyleBackColor = true;
+            this.submit.Click += new System.EventHandler(this.submit_Click);
+            // 
+            // checkBoxContainer
+            // 
+            this.checkBoxContainer.Location = new System.Drawing.Point(372, 44);
+            this.checkBoxContainer.Name = "checkBoxContainer";
+            this.checkBoxContainer.Size = new System.Drawing.Size(548, 84);
+            this.checkBoxContainer.TabIndex = 5;
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(342, 170);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(129, 52);
+            this.button2.TabIndex = 6;
+            this.button2.Text = "Stop Connection";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.stop_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(539, 170);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(121, 52);
+            this.button3.TabIndex = 7;
+            this.button3.Text = "Start Connection";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.start_Click);
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.curveGridView);
+            this.panel1.Location = new System.Drawing.Point(88, 284);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(879, 296);
+            this.panel1.TabIndex = 8;
+            // 
+            // curveGridView
+            // 
+            this.curveGridView.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.curveGridView.Location = new System.Drawing.Point(63, 16);
+            this.curveGridView.Name = "curveGridView";
+            this.curveGridView.RowTemplate.Height = 28;
+            this.curveGridView.Size = new System.Drawing.Size(439, 267);
+            this.curveGridView.TabIndex = 0;
+            // 
+            // xml_Export
+            // 
+            this.xml_Export.Location = new System.Drawing.Point(708, 170);
+            this.xml_Export.Name = "xml_Export";
+            this.xml_Export.Size = new System.Drawing.Size(148, 52);
+            this.xml_Export.TabIndex = 9;
+            this.xml_Export.Text = "Export Grid Data To XML";
+            this.xml_Export.UseVisualStyleBackColor = true;
+            this.xml_Export.Click += new System.EventHandler(this.xml_Export_Click);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(147, 252);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(170, 20);
+            this.label3.TabIndex = 10;
+            this.label3.Text = "Selected Curve Details";
+            // 
+            // messageBox
+            // 
+            this.messageBox.AutoSize = true;
+            this.messageBox.Location = new System.Drawing.Point(650, 252);
+            this.messageBox.Name = "messageBox";
+            this.messageBox.Size = new System.Drawing.Size(0, 20);
+            this.messageBox.TabIndex = 11;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1144, 636);
+            this.Controls.Add(this.messageBox);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.xml_Export);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.checkBoxContainer);
+            this.Controls.Add(this.submit);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.panel1.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.curveGridView)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button submit;
+        private System.Windows.Forms.Panel checkBoxContainer;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.DataGridView curveGridView;
+        private System.Windows.Forms.Button xml_Export;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label messageBox;
+    }
+}
+
---
+++ PetroClient/Form1.cs
@@ -0,0 +1,363 @@
+﻿#region Assemblies
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Net;
+using System.Net.Sockets;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Xml;
+#endregion
+
+
+namespace PetroClient
+{
+    public partial class Form1 : Form
+    {
+        System.Net.Sockets.TcpClient clientSocket;
+        IPAddress ip = Dns.GetHostEntry("localhost").AddressList[0];
+        CancellationTokenSource tokenSource;
+
+        public Form1()
+        {            
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            StartConnection();
+            label1.Text = "Client Socket Program - Server Connected ...";
+            List<string> curveHeader = GetResponse("CurveHeader").Split(',').ToList();
+            GenerateDynamicCheckbox(curveHeader);
+            messageBox.ForeColor = Color.Red;
+        }        
+
+
+        /// <summary>
+        /// Submit button click
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void submit_Click(object sender, EventArgs e)
+        {
+            curveGridView.Columns.Clear();
+            messageBox.Text = "";            
+            List<string> curveSelected = CurveSelected();
+            if(curveSelected.Count > 0 && clientSocket != null && clientSocket.Connected)
+            { 
+                //to cancel a task, if connection stop requested by client.
+                tokenSource = new CancellationTokenSource();
+                var token = tokenSource.Token;
+
+                int gridColumn = curveSelected.Count + 1;
+                curveGridView.ColumnCount = gridColumn;
+
+                curveGridView.Columns[0].Name = "Index";
+                curveGridView.Columns[0].ValueType = typeof(long);
+                for (int i = 1; i <= curveSelected.Count; i++)
+                {
+                    curveGridView.Columns[i].Name = curveSelected[i - 1];
+                    curveGridView.Columns[i].ValueType = typeof(double);
+                }
+                curveGridView.AutoResizeColumns();
+                curveGridView.AutoSizeColumnsMode = DataGridViewAutoSizeColumnsMode.AllCells;
+
+                //to execute the function using thread
+                Task.Run(() =>
+                {
+                    string indexList = GetResponse("Index");
+
+                    WriteToDataGrid(indexList, "Index");
+                    for (int i = 0; i < curveSelected.Count; i++)
+                    {
+                        if (clientSocket.Connected)
+                        {
+                            string returnData = GetResponse(curveSelected[i]);
+                            WriteToDataGrid(returnData, curveSelected[i]);
+                        }
+                    }
+                }, token);
+            }
+            else
+            {
+                if(clientSocket !=null && !clientSocket.Connected)
+                {                    
+                    messageBox.Text = "Please start the connection";
+                }
+                else
+                {
+                    messageBox.Text = "Please select any curve for dispaying data";
+                }                
+            }            
+        }
+
+        /// <summary>
+        /// To stop connection click
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void stop_Click(object sender, EventArgs e)
+        {
+            // to check for an active task and stop if exist
+            if (tokenSource != null && !tokenSource.IsCancellationRequested)
+            {
+                tokenSource.Cancel();
+                tokenSource.Dispose();
+                tokenSource = null;
+            }
+            if (clientSocket.Connected)
+            {
+                clientSocket.Close();                
+            }
+        }
+
+        /// <summary>
+        /// Start connection click handler
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void start_Click(object sender, EventArgs e)
+        {
+            if (clientSocket != null && !clientSocket.Connected)
+            {
+                StartConnection();                
+            }
+        }
+
+
+        /// <summary>
+        /// To export grid data to xml file
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void xml_Export_Click(object sender, EventArgs e)
+        {
+            if(curveGridView != null && curveGridView.Columns.Count > 0 && curveGridView.Rows.Count >0)
+            {
+
+                XmlTextWriter writer = new XmlTextWriter(@"D:\Study\Curves.xml", System.Text.Encoding.UTF8);
+                writer.WriteStartDocument(true);
+                writer.Formatting = Formatting.Indented;
+                writer.Indentation = 2;
+                writer.WriteStartElement("log");
+
+                DataTable gridData = new DataTable();
+                List<string> curveHeader = new List<string>();
+
+                foreach (DataGridViewColumn col in curveGridView.Columns)
+                {
+                    gridData.Columns.Add(col.Name);
+                    curveHeader.Add(col.Name);
+                    var maxIndex = curveGridView.Rows.Cast<DataGridViewRow>()
+                            .Max(r => Convert.ToDouble(string.IsNullOrEmpty(Convert.ToString(r.Cells[col.Name].Value)) ? 0 : r.Cells[col.Name].Value));
+
+                    var minIndex = curveGridView.Rows.Cast<DataGridViewRow>()
+                            .Min(r => Convert.ToDouble(string.IsNullOrEmpty(Convert.ToString(r.Cells[col.Name].Value)) ? 0 : r.Cells[col.Name].Value));
+
+                    createNode(col.ValueType.ToString(), col.Name, Convert.ToInt32(maxIndex), Convert.ToInt32(minIndex), "logCurveInfo", "typeLogData", writer);
+                }
+
+                writer.WriteStartElement("logData");
+                createNode("", string.Join(",", curveHeader), 0, 0, "header", "", writer);
+                List<string> curveData;
+                foreach (DataGridViewRow row in curveGridView.Rows)
+                {
+                    if(row.Cells[0].Value != null)
+                    {
+                        curveData = new List<string>();
+                        DataRow dRow = gridData.NewRow();
+                        foreach (DataGridViewCell cell in row.Cells)
+                        {
+                            dRow[cell.ColumnIndex] = cell.Value;
+                            curveData.Add(Convert.ToString(cell.Value));
+                        }
+                        gridData.Rows.Add(dRow);
+                        createNode("", string.Join(",", curveData), 0, 0, "data", "", writer);
+                    }                    
+                    
+                }
+                writer.WriteEndElement();
+                writer.WriteEndElement();
+                writer.WriteEndDocument();
+                writer.Close();
+            }
+            else
+            {
+                messageBox.Text = "Please Select Curves Details First";
+            }
+
+        }
+
+
+        /// <summary>
+        /// Curve Selected by client
+        /// </summary>
+        /// <returns></returns>
+        private List<string> CurveSelected()
+        {
+            List<string> curveSelected = new List<string>();
+            int count = 0;
+            foreach (Control control in checkBoxContainer.Controls)
+            {
+                count++;
+                CheckBox cb = checkBoxContainer.Controls["cb_" + count] as CheckBox;
+                if (cb != null && cb.Checked)
+                {
+                    curveSelected.Add(cb.Text);
+                }
+            }
+            return curveSelected;
+        }
+
+        /// <sumTmary>
+        /// To get the response from server
+        /// </summary>
+        /// <param name="selectedValue"></param>
+        /// <returns></returns>
+        private string GetResponse(string selectedValue)
+        {
+            try
+            {
+                NetworkStream serverStream = clientSocket.GetStream();
+                byte[] outStream = System.Text.Encoding.ASCII.GetBytes(selectedValue + "$");
+                serverStream.Write(outStream, 0, outStream.Length);
+                serverStream.Flush();
+
+                byte[] inStream = new byte[1500];
+                serverStream.Read(inStream, 0, inStream.Length);
+                return System.Text.Encoding.ASCII.GetString(inStream).Replace("\0", "");
+            }
+            catch (Exception ex)
+            {
+                return "";
+            }
+
+        }
+
+        /// <summary>
+        /// To add data to text box
+        /// </summary>
+        /// <param name="mesg"></param>
+        private void WriteToDataGrid(string response, string curve = "")
+        {
+            //To check whether the action is performed by main thread or child thread
+            if (InvokeRequired)
+            {
+                //to execute the code in main thread context
+                Invoke((MethodInvoker)delegate ()
+                {                    
+                    if(curve != "")
+                    {
+                        AddDataToGrid(response, curve);
+                    }
+                });
+            }
+            else
+            {                
+                if (curve != "" )
+                {
+                    AddDataToGrid(response, curve);
+                }
+            }
+        }
+
+        /// <summary>
+        /// To add data to grid view
+        /// </summary>
+        /// <param name="response"></param>
+        /// <param name="curve"></param>
+        private void AddDataToGrid(string response, string curve)
+        {
+            string[] curves = response.Split(',');
+            int i = 0;
+            if (curve == "Index")
+            {
+                foreach (string curveValue in curves)
+                {
+                    curveGridView.Rows.Add(curveValue);
+                }
+            }
+            else
+            {
+                foreach (string curveValue in curves)
+                {
+                    curveGridView.Rows[i].Cells[curve].Value = curveValue;
+                    i++;
+                }
+            }
+        }
+
+        /// <summary>
+        /// To generate check box based on the Curve header 
+        /// </summary>
+        /// <param name="mylist"></param>
+        private void GenerateDynamicCheckbox(List<string> mylist)
+        {
+            CheckBox box;
+            for (int i = 1; i < mylist.Count; i++)
+            {
+                box = new CheckBox();
+                box.Name = "cb_" + i;
+                box.Tag = mylist[i];
+                box.Text = mylist[i];
+                box.AutoSize = true;
+                box.Location = new Point(i * 50, 40); //horizontal
+                checkBoxContainer.Controls.Add(box);
+            }
+        }
+        
+        /// <summary>
+        /// To start TCP Client connection
+        /// </summary>
+        private void StartConnection()
+        {
+            clientSocket = new System.Net.Sockets.TcpClient();
+            clientSocket.Connect("127.0.0.1", 7633);
+        }
+
+        /// <summary>
+        /// To add child node to xml file
+        /// </summary>
+        /// <param name="nodeDataType"></param>
+        /// <param name="nodeData"></param>
+        /// <param name="maxIndex"></param>
+        /// <param name="minIndex"></param>
+        /// <param name="childNode"></param>
+        /// <param name="childNode2"></param>
+        /// <param name="writer"></param>
+        private void createNode(string nodeDataType, string nodeData, int maxIndex, int minIndex, string childNode, string childNode2, XmlTextWriter writer)
+        {
+            writer.WriteStartElement(childNode);
+            if(nodeDataType != "")
+            {
+                writer.WriteAttributeString("id", nodeData);
+            }
+            else
+            {
+                writer.WriteString(nodeData);
+            }            
+
+            if (maxIndex > 0)
+            {
+                writer.WriteStartElement("minIndex");
+                writer.WriteString(minIndex.ToString());
+                writer.WriteEndElement();
+                writer.WriteStartElement("maxIndex");
+                writer.WriteString(maxIndex.ToString());
+                writer.WriteEndElement();
+            }
+            if(childNode2 != "")
+            {
+                writer.WriteStartElement(childNode2);
+                writer.WriteString(nodeDataType);
+                writer.WriteEndElement();
+            }
+                        
+            writer.WriteEndElement();
+        }
+
+    }
+}
